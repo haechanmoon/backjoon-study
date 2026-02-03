4b7970bf3e323fdd1d547612508c11a4a0d4010c
Pino Moon <timmymoon@naver.com>
2026-02-03 23:59:33 +0900

    [Gold III] Title: 나머지 합, Time: 424 ms, Memory: 120688 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        long result = 0;
+        long[] S = new long[N + 1];
+        long[] count = new long[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            S[i] = (S[i - 1] + Integer.parseInt(st.nextToken())) % M;
+            
+            if (S[i] == 0) {
+                result++;
+            }
+            count[(int) S[i]]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (count[i] > 1) {
+                result += (count[i] * (count[i] - 1) / 2);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
