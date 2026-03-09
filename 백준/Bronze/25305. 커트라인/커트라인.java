8169bec9924bee4ef47caef73ad69d8d0cc6d0c9
Pino Moon <timmymoon@naver.com>
2026-03-09 22:46:21 +0900

    [Bronze II] Title: 커트라인, Time: 116 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Integer[] scores = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(scores, Collections.reverseOrder());
+
+        System.out.println(scores[k - 1]);
+    }
+}
