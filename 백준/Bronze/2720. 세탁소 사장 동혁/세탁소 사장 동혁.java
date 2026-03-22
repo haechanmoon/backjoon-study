3d3fb61f8ded5cdeb2facbb6749cab9dd06d9c83
Pino Moon <timmymoon@naver.com>
2026-03-22 23:25:43 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            int C = Integer.parseInt(br.readLine());
+
+            int quarter = C / 25;
+            C %= 25;
+
+            int dime = C / 10;
+            C %= 10;
+
+            int nickel = C / 5;
+            C %= 5;
+
+            int penny = C;
+
+            sb.append(quarter).append(" ")
+              .append(dime).append(" ")
+              .append(nickel).append(" ")
+              .append(penny).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
