3012be269f01b24f5a567dfe658c3be7dbce6ed9
Pino Moon <timmymoon@naver.com>
2026-03-28 22:50:02 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (min > i) {
+                    min = i;
+                }
+            }
+        }
+        
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
