d9660d19639160f8eb84ace9cdbc757616a2f6f8
Pino Moon <timmymoon@naver.com>
2026-03-29 23:45:47 +0900

    [Bronze I] Title: 최소공배수, Time: 116 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int gcd = getGCD(a, b);
+            sb.append((a * b) / gcd).append('\n');
+        }
+        
+        System.out.print(sb);
+    }
+    
+    public static int getGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return getGCD(b, a % b);
+    }
+}
