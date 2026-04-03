636642edb0ecd6fb85d1edd54932ff12b2406ca7
Pino Moon <timmymoon@naver.com>
2026-04-03 21:22:33 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sidePoints = (int)Math.pow(2, N) + 1;
+        System.out.println(sidePoints * sidePoints);
+    }
+}
