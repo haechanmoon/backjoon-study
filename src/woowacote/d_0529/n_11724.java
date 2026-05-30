cacfae7438b0b4129b768a50a86bbddc02db9c0d
haechanmoon <moonhc.dev@gmail.com>
2026-05-30 23:54:53 +0900

    2025_05_28 실버5 연결요소

---
+++ src/woowacote/d_0529/n_11724.java
@@ -0,0 +1,15 @@
+package woowacote.d_0529;
+
+public class n_11724 {
+    /*
+    1. n, m 저장
+    2. A(그래프 데이터 저장 인접 리스트)
+    3. visited(방문 기록 저장 배열)
+    4. for(n만큼) -> 각 리스트 초기화
+    5. for(m만큼) -> A 에 그래프 데이터 저장
+    6. for(n만큼) -> if(방문하지 않은 노드가 있으면) -> 연결요소 ++, dfs실행
+    7. dfs if(현재==방문) visited
+     */
+    
+
+}
