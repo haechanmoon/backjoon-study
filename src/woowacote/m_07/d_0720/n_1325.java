c1fedd7cae3a197919a46332c86c04e8513e1543
haechanmoon <moonhc.dev@gmail.com>
2026-07-20 22:33:44 +0900

    2026_07_20 실버1 효율적으로 해킹하기

---
+++ src/woowacote/m_07/d_0720/n_1325.java
@@ -0,0 +1,44 @@
+package woowacote.m_07.d_0720;
+
+/*
+효율적으로 해킹하기
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class n_1325 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        ArrayList<Integer>[] arr = new ArrayList[M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            arr[u].add(v);
+        }
+        bfs(1);
+
+    }
+
+    private static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        int max = 0;
+        while (!queue.isEmpty()) {
+
+        }
+    }
+}
