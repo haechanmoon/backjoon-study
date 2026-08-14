9498b5e4738f617dea8e6086cdd2d4fb10230879
haechanmoon <moonhc.dev@gmail.com>
2026-08-14 17:47:52 +0900

    2026_08_12 골드5 최소 경로 구하기

--- src/woowacote/m_08/d_0812/n_1753.java
+++ src/woowacote/m_08/d_0812/n_1753.java
@@ -8,26 +8,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class n_1753 {
+
+    private static int N, M, K;
+    private static int[] distance;
+    private static boolean[] visited;
+    private static ArrayList<Edge>[] list;
+    private static PriorityQueue<Edge> q = new PriorityQueue<Edge>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
 
-        int[] result = new int[N + 1];
-        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+        distance = new int[N + 1];
+        visited = new boolean[N + 1];
+        list = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+            list[i] = new ArrayList<>();
         }
         for (int i = 1; i <= N; i++) {
-            result[i] = Integer.MAX_VALUE;
+            distance[i] = Integer.MAX_VALUE;
         }
-        result[K] = 0;
-
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list[u].add(new Edge(v, w));
+        }
+        q.add(new Edge(K, 0));
+        distance[K] = 0;
+        while (!q.isEmpty()) {
+            Edge current = q.poll();
+            int c_v = current.vertex;
+            if (visited[c_v]) {
+                continue;
+            }
+            visited[c_v] = true;
+            for (int i = 0; i < list[c_v].size(); i++) {
+                Edge temp = list[c_v].get(i);
+                int next = temp.vertex;
+                int value = temp.value;
+                if (distance[next] > distance[c_v] + value) {
+                    distance[next] = value + distance[c_v];
+                    q.add(new Edge(next, distance[next]));
+                }
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) {
+                System.out.println(distance[i]);
+            } else {
+                System.out.println("INF");
+            }
+        }
     }
 }
 
