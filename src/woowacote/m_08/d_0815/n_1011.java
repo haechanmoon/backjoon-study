ef23b85104719201efd16f0e55e6bebcf24538db
haechanmoon <moonhc.dev@gmail.com>
2026-08-16 23:55:34 +0900

    2026_08_16 골드5 최소 비용 구하기

--- src/woowacote/m_08/d_0815/n_1011.java
+++ src/woowacote/m_08/d_0815/n_1011.java
@@ -1,4 +1,76 @@
 package woowacote.m_08.d_0815;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Node implements Comparable<Node> {
+    int to;
+    int weight;
+
+    public Node(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Integer.compare(this.weight, o.weight);
+    }
+}
+
 public class n_1011 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        ArrayList<Node>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[u].add(new Node(v, w));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int now = current.to;
+            int cost = current.weight;
+
+            if (dist[now] < cost) {
+                continue;
+            }
+
+            for (Node next : graph[now]) {
+                if (dist[next.to] > cost + next.weight) {
+                    dist[next.to] = cost + next.weight;
+                    pq.offer(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+
+        System.out.println(dist[end]);
+    }
 }
